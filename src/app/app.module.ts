9a82d73177afb4b57a9ba78f6cf0dee3ab02fb9a
joaoabjr <junioralmeida_15@hotmail.com>
2023-03-29 11:55:22 -0300

    card and pokemon-list structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		PokemonListComponent,
+		PokemonCardComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+
+@Component({
+	selector: 'app-pokemon-list',
+	templateUrl: './pokemon-list.component.html',
+	styleUrls: ['./pokemon-list.component.scss'],
+})
+export class PokemonListComponent {}
